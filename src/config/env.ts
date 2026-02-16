0c995f0cb5c5555cbc6a48dcb96d2c3a8df55335
shinhanseo <imkara1@naver.com>
2026-02-16 16:28:27 +0900

    Fix : 백엔드 배포에 따른 API 주소 변경

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ type ExpoConfigExtra = {
   API_BASE_URL?: string;
 };
 
-const DEFAULT_API_BASE_URL = "http://192.168.0.30:4000";
+const DEFAULT_API_BASE_URL = "http://3.26.43.127:4000";
 
 export const API_BASE_URL =
   (Constants.expoConfig?.extra as ExpoConfigExtra | undefined)?.API_BASE_URL ?? DEFAULT_API_BASE_URL;
