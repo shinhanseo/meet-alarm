b0bfad4685008c586592df7c93d14f0f5581ec60
shinhanseo <imkara1@naver.com>
2026-02-19 13:05:37 +0900

    구글 맵키 환경변수 처리

---
+++ app.config.js
@@ -0,0 +1,77 @@
+// app.config.js
+export default {
+  expo: {
+    name: "meet-alarm",
+    slug: "meet-alarm",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "meetalarm",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+
+    ios: {
+      supportsTablet: true,
+    },
+
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      permissions: ["android.permission.CAMERA", "android.permission.RECORD_AUDIO"],
+      package: "com.imkara1.meetalarm",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+      [
+        "expo-camera",
+        {
+          cameraPermission: "출발 인증 사진을 위해 카메라 접근이 필요해요.",
+        },
+      ],
+      "@react-native-community/datetimepicker",
+      "expo-secure-store",
+    ],
+
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+
+    extra: {
+      API_BASE_URL: "https://api.meetalarm.kr",
+      router: {},
+      eas: {
+        projectId: "82825f0b-c0e8-4629-8b08-257b68a76ff0",
+      },
+    },
+  },
+};
