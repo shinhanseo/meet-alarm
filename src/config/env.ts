c8be4c33693b4bf9603efb5d45c84e892005932d
shinhanseo <imkara1@naver.com>
2026-02-19 11:43:08 +0900

    Chore : 백엔드 https로 주소 변경에 따른 환경변수 수정

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,10 +1,2 @@
-import Constants from "expo-constants";
-
-type ExpoConfigExtra = {
-  API_BASE_URL?: string;
-};
-
-const DEFAULT_API_BASE_URL = "http://3.26.43.127:4000";
-
 export const API_BASE_URL =
-  (Constants.expoConfig?.extra as ExpoConfigExtra | undefined)?.API_BASE_URL ?? DEFAULT_API_BASE_URL;
+  process.env.EXPO_PUBLIC_API_BASE_URL ?? "https://api.meetalarm.kr";
